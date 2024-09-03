2dfdfb66fbc5b4809f2350b48b6dd339a0386407
Koushik Nikkam K <102957505+Koushik-Acespire@users.noreply.github.com>
2024-09-03 22:53:55 +0530

    Update settings.py

--- Chain_Shield/settings.py
+++ Chain_Shield/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-a&(c3sm1j$=iad2=d^3_-anp=3b)*j(z3b@x)65cu$#5tl6!om
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ['.vercel.app']
+# ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
